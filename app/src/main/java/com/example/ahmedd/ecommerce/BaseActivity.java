3fb9e9c0531e992d02ba724f2390e0111a488d2c
Ahmed <ahmedmohamedaneng@gmail.com>
2018-09-15 22:07:42 +0200

    Initial commit

---
+++ app/src/main/java/com/example/ahmedd/ecommerce/BaseActivity.java
@@ -0,0 +1,12 @@
+package com.example.ahmedd.ecommerce;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class BaseActivity extends AppCompatActivity {
+
+    AppCompatActivity activity;
+
+    public BaseActivity(){
+        activity = this;
+    }
+}
